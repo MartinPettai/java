cacc6f20ff085b0cc963608e5e5c177b1df68a77
MartinPettai <73310313+MartinPettai@users.noreply.github.com>
2022-09-21 20:01:45 +0300

    Add files via upload

---
+++ harjutus1.java
@@ -0,0 +1,33 @@
+//Martin Pettai
+//IT21
+
+public class harjutus1 {
+    
+ /**
+ * @param args
+ */
+public static void main(String[] args) {
+
+    String nimi = new String();
+    nimi = "Martin Pettai";  
+    String kursus = "IT21";
+    String hyydnimi= new String();
+    hyydnimi="Pettai";
+    int vanus= 17;
+    float pikkus=1.75f;
+    
+    
+    System.out.println("Minu nimi on "+nimi+", 6pin kursusel "+kursus+ ". Olen "+vanus+" aastat vana, pikkuseks on "+pikkus+" ja hyydnimeks on mul "+hyydnimi);
+
+
+
+    
+
+    
+
+    
+
+
+    }
+}
+   
