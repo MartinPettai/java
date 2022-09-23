3032fda62524a8d3a38fcf2f3a0549652db1c627
MartinPettai <73310313+MartinPettai@users.noreply.github.com>
2022-09-23 16:04:30 +0300

    Add files via upload

---
+++ harjutus2.java
@@ -0,0 +1,12 @@
+public class harjutus2 {
+ 
+ public static void main(String[] args) {
+
+int toll= 1;
+double meeter_tollides= 0.0254;
+double vastus = toll*meeter_tollides;
+
+System.out.print("Yks toll meetrites on: "+ vastus+ " meetrit!");
+
+ }
+}
